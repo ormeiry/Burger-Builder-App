24330e60cb43be902b496468416d930d0c453a0b
Or Meiry <ormeiry2411@gmail.com>
2020-08-24 21:09:21 +0300

    added a Logo component with styling. fixed typo when importing css module file for Toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Aux from "../../hoc/Auxiliary";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
 import classes from "./Layout.module.css";
 
 const Layout = (props) => (
   <Aux>
-    <div>Toolbar, SideDrawer, Backdrop</div>
+    <Toolbar />
     <main className={classes.Content}>{props.children}</main>
   </Aux>
 );
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,10 +1,11 @@
 import React from "react";
-import classes from "./Tollbar.module.css";
+import Logo from "../../Logo/Logo";
+import classes from "./Toolbar.module.css";
 
 const toolbar = (props) => (
   <header className={classes.Toolbar}>
     <div>Menu</div>
-    <div>Logo</div>
+    <Logo />
     <nav>...</nav>
   </header>
 );
