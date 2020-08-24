813a00e0b25d324dc95f164ff21c16e1942a2b19
Or Meiry <ormeiry2411@gmail.com>
2020-08-24 20:54:34 +0300

    Added initial toolbar component with css

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./Tollbar.module.css";
+
+const toolbar = (props) => (
+  <header className={classes.Toolbar}>
+    <div>Menu</div>
+    <div>Logo</div>
+    <nav>...</nav>
+  </header>
+);
+
+export default toolbar;
