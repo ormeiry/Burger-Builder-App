f72734f670bfbaef4b3c65fd9979241fe4102cfc
Or Meiry <ormeiry2411@gmail.com>
2020-08-26 21:21:34 +0300

    Added the backdrop to the mobile layout.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import Aux from "../../hoc/Auxiliary";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import classes from "./Layout.module.css";
 import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-const Layout = (props) => (
-  <Aux>
-    <Toolbar />
-    <SideDrawer />
-    <main className={classes.Content}>{props.children}</main>
-  </Aux>
-);
+class Layout extends Component {
+  state = {
+    showSideDrawer: true,
+  };
+
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
+
+  render() {
+    return (
+      <Aux>
+        <Toolbar />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerClosedHandler}
+        />
+        <main className={classes.Content}>{this.props.children}</main>
+      </Aux>
+    );
+  }
+}
 
 export default Layout;
